c773854202866ffc4c8d1d51c8946979de1519cf
Guillaume NICOLAS <guillaume.nicolas@abtasty.com>
2024-10-25 11:04:49 +0200

    feat: add iframes in abtest page

--- src/app/abtest/page.tsx
+++ src/app/abtest/page.tsx
@@ -2,10 +2,12 @@ import Head from "next/head";
 
 export default function ABTestingPage() {
   return (
-    <div className="w-full h-screen bg-primary text-white ">
+    <div className="w-full h-screen bg-primary text-white flex flex-col">
       <h1 className="text-3xl text-center p-10 font-bold">
         Demo page to do some ABTesting
       </h1>
+      <iframe src="https://beta.nigui.fr/search" />
+      <iframe src="https://beta.nigui.fr/random" className="flex-grow" />
     </div>
   );
 }
