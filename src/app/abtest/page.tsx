59649d4a3d8379bc4455e224fc45a92540a3e26c
Guillaume NICOLAS <guillaume.nicolas@abtasty.com>
2024-10-25 14:05:07 +0200

    fix: grow iframe in abtest for styling

--- src/app/abtest/page.tsx
+++ src/app/abtest/page.tsx
@@ -4,7 +4,7 @@ export default function ABTestingPage() {
       <h1 className="text-3xl text-center p-10 font-bold">
         Demo page to do some ABTesting
       </h1>
-      <iframe src="https://beta.nigui.fr/search" />
+      <iframe src="https://beta.nigui.fr/search" className="flex-grow" />
     </div>
   );
 }
