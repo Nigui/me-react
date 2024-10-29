ba3ebc8ae171d1a48b971f21dae07bf718fe4bc5
Guillaume NICOLAS <guillaume.nicolas@abtasty.com>
2024-10-29 21:19:03 +0100

    chore: bump react to v19

--- src/app/home/components/CardButton.component.tsx
+++ src/app/home/components/CardButton.component.tsx
@@ -1,7 +1,7 @@
 import { ReactElement, ReactNode, cloneElement, createElement } from "react";
 
 type CardButtonDefaultProps = {
-  icon: ReactElement;
+  icon: ReactElement<any>;
   label: string;
 };
 
