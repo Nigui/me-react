96fc9535747018c147f5b4b5cc3a7ec483d418f8
Guillaume NICOLAS <guillaume.nicolas@abtasty.com>
2025-01-17 11:57:03 +0100

    chore: use custom domain abtasty tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
       <head>
         <Script
           type="text/javascript"
-          src="https://try.abtasty.com/36ad5e4d1a4e0f94c06814a7a02d3c3f.js"
+          src="https://customdomain-staging.abtasty.io/36ad5e4d1a4e0f94c06814a7a02d3c3f.js"
         />
       </head>
       <body>
