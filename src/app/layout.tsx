2889874d03b8f44478163f9f295652b6b32f9801
Guillaume NICOLAS <guillaume.nicolas@abtasty.com>
2025-08-14 15:51:49 +0200

    feat: use dev try 2 abtasty tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
       <head>
         <Script
           type="text/javascript"
-          src="https://staging-try.nigui.fr/36ad5e4d1a4e0f94c06814a7a02d3c3f.js"
+          src="https://dev-try-2.nigui.fr/36ad5e4d1a4e0f94c06814a7a02d3c3f.js"
         />
       </head>
       <body>
